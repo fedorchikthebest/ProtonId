658bc0576d2e10b659d6e97111a1138fa565f386
user <1@1.1>
2023-12-28 10:11:37 +0300

    Initial commit

---
+++ data/db_session.py
@@ -0,0 +1,38 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+import sqlalchemy.ext.declarative as dec
+
+SqlAlchemyBase = dec.declarative_base()
+
+__factory = None
+db_sess = None
+
+
+def global_init(db_file):
+    global __factory, db_sess
+
+    if __factory:
+        db_sess = __factory()
+        return
+
+    if not db_file or not db_file.strip():
+        raise Exception("Необходимо указать файл базы данных.")
+
+    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    print(f"Подключение к базе данных по адресу {conn_str}")
+
+    engine = sa.create_engine(conn_str, echo=False)
+    __factory = orm.sessionmaker(bind=engine)
+
+    from . import __all_models
+
+    SqlAlchemyBase.metadata.create_all(engine)
+    db_sess = __factory()
+
+
+def create_session() -> Session:
+    global __factory
+    if __factory is None:
+        return None
+    return __factory()
---
+++ main.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
